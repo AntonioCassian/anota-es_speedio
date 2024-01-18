b784f015566d31cc64478e58775619ccd263ce5a
AntonioCassian <antoniomart849@gmail.com>
2024-01-18 11:23:49 -0300

    feat: configuration e first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+  <h3>Anotações</h3>
+  <button>
+    <span>+</span>
+    Criar anotações
+  </button>
+</template>
+
+<style scoped>
+
+</style>
