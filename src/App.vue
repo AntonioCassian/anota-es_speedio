c56c685c6eb148768fd17920f34e992b2dae115e
AntonioCassian <antoniomart849@gmail.com>
2024-01-18 20:22:12 -0300

    feat: first template

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,80 @@
 </script>
 
 <template>
-  <h3>Anotações</h3>
-  <button>
-    <span>+</span>
-    Criar anotações
-  </button>
+  <div class="ceter">
+    <div class="centere">
+      <div class="logo">
+        <div class="icon-logo">
+          <i class="pi pi-paperclip"></i>
+        </div>
+        <div class="text-logo">
+          Anotações
+          <span>by Speedio</span>
+        </div>
+      </div>
+      <button class="btn">
+        <i class="pi pi-plus"></i>
+        Criar anotação
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.ceter {
+  width: 500px;
+  height: 400px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #F4F3F8;
+}
 
+.centere {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 60px;
+}
+.logo{
+  display: inline-flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    /* align-content: center; */
+    align-items: center;
+    gap: 12px;
+}
+
+.icon-logo i {
+  color: #B7BBBE;
+  font-size: 45px;
+  transform: scale(-1);
+}
+
+.text-logo {
+  font-size: 22px;
+  font-weight: 900;
+  color: #1295FD;
+  display: inline-grid;
+  justify-items: center;
+}
+
+.text-logo span {
+  font-size: 12px;
+  color: #B7BBBE;
+}
+
+.btn {
+  position: relative;
+  background: #1295FD;
+  color: #fafafa;
+  border: none;
+  padding: 15px;
+  border-radius: 30px;
+}
+
+.btn i {
+  margin-right: 4px;
+}
 </style>
---
+++ src/components/titulo/Titulo.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        {{ props.title }}
+    </div>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps(['title'])
+</script>
+
+<style scoped>
+</style>
